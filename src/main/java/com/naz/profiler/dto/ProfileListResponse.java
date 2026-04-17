aad07967098b58f8d13eac4d3fa18b7028ba8e96
mac <chinaza.herbert@decagon.dev>
2026-04-17 23:51:51 +0100

    update..

--- src/main/java/com/naz/profiler/dto/ProfileListResponse.java
+++ src/main/java/com/naz/profiler/dto/ProfileListResponse.java
@@ -5,9 +5,9 @@ import java.util.List;
 public record ProfileListResponse(
         String status,
         Integer count,
-        List<ProfileList> profiles
+        List<ProfileList> profile
 ) implements ApiResponse {
-    public ProfileListResponse(Integer count, List<ProfileList> profiles){
-        this("success",count, profiles);
+    public ProfileListResponse(Integer count, List<ProfileList> profile){
+        this("success",count, profile);
     }
 }
