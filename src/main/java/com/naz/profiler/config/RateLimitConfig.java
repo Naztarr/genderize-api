0d834d9e8c247fa502f51f9f72747c54cd8fbc65
mac <chinaza.herbert@decagon.dev>
2026-05-05 07:44:26 +0100

    refactor: refactored ratelimitconfig to resolve redis host issues

--- src/main/java/com/naz/profiler/config/RateLimitConfig.java
+++ src/main/java/com/naz/profiler/config/RateLimitConfig.java
@@ -11,26 +11,45 @@ import io.lettuce.core.codec.RedisCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RateLimitConfig {
-    @Value("${spring.data.redis.url:redis://localhost:6379}")
-    private String redisUrl;
+//    @Value("${spring.data.redis.url:redis://localhost:6379}")
+//    private String redisUrl;
 
 //    private String redisUrl = "redis://localhost:6379";
 
-    @Bean
-    public RedisClient redisClient() {
-        return RedisClient.create(redisUrl);
-    }
+//    @Bean
+//    public RedisClient redisClient() {
+//        return RedisClient.create(redisUrl);
+//    }
+
+//    @Bean
+//    public ProxyManager<byte[]> proxyManager(RedisClient redisClient) {
+//        StatefulRedisConnection<byte[], byte[]> connection = redisClient
+//                .connect(RedisCodec.of(new ByteArrayCodec(), new ByteArrayCodec()));
+//
+//        ClientSideConfig clientSideConfig = ClientSideConfig.getDefault()
+//                .withExpirationAfterWriteStrategy(ExpirationAfterWriteStrategy.none());
+//
+//        return LettuceBasedProxyManager.builderFor(connection)
+//                .withClientSideConfig(clientSideConfig)
+//                .build();
+//    }
 
     @Bean
-    public ProxyManager<byte[]> proxyManager(RedisClient redisClient) {
+    public ProxyManager<byte[]> proxyManager(LettuceConnectionFactory connectionFactory) {
+        // This pulls the connection from the Factory Spring already set up
+        // using the spring.data.redis.url property above.
+        RedisClient redisClient = (RedisClient) connectionFactory.getNativeClient();
         StatefulRedisConnection<byte[], byte[]> connection = redisClient
                 .connect(RedisCodec.of(new ByteArrayCodec(), new ByteArrayCodec()));
 
         ClientSideConfig clientSideConfig = ClientSideConfig.getDefault()
-                .withExpirationAfterWriteStrategy(ExpirationAfterWriteStrategy.none());
+                .withExpirationAfterWriteStrategy(ExpirationAfterWriteStrategy.fixedTimeToLive(Duration.ofHours(1)));
 
         return LettuceBasedProxyManager.builderFor(connection)
                 .withClientSideConfig(clientSideConfig)
